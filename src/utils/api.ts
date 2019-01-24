de59d6ade5b09f11ce475e233f59e7345464b8d7
wuxiaoran <wxrbw@qq.com>
2019-01-24 18:29:20 +0800

    chore:

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -23,7 +23,7 @@ const removePending = (config: any) => {
     const item: any = p;
     const list: any = pending[p];
     // 当前请求在数组中存在时执行函数体
-    if (list.url === `${config.url}&request_type=config.method`) {
+    if (list.url === `${config.url}&request_type=${config.method}`) {
       // 执行取消操作
       list.cancel();
       // 从数组中移除记录
@@ -42,7 +42,7 @@ api.interceptors.request.use(
     removePending(config);
     config.cancelToken = new cancelToken((c) => {
       pending.push({
-        url: `${config.url}&request_type=config.method`,
+        url: `${config.url}&request_type=${config.method}`,
         cancel: c });
     });
     return config;
